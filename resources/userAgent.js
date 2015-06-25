007e94383757b97f95832790cf77658bd84a967e
Nick Loveridge <nick@fusemedia.ca>
2015-06-25 15:36:24 -0700

    pointer

--- resources/userAgent.js
+++ resources/userAgent.js
@@ -1,13 +1,13 @@
-(function(){
+(function() {
 
   var ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X; en-us) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53';
 
-  navigator.__defineGetter__('userAgent', function(){
+  navigator.__defineGetter__('userAgent', function() {
     return ua;
   });
 
   // Loaded before element.
-  if(!navigator.userAgent.match(/iPhone/i)) {
+  if (!navigator.userAgent.match(/iPhone/i)) {
     var alert = document.getElementById('alert');
     console.log(alert);
     alert.setAttribute('style', 'display:block;');
